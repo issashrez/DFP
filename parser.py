2a6d49299a591961ee03d78eae3f8a776944070d
Shreyas Biju Nair <149809129+issashrez@users.noreply.github.com>
2025-03-05 13:24:59 +0000

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,10 +1,8 @@
 from zat.log_to_dataframe import LogToDataFrame
 
-# Create an instance of LogToDataFrame
+
 log_to_df = LogToDataFrame()
 
-# Replace this path with the path to your Zeek log file
 df = log_to_df.create_dataframe('E:/net_log/zeek/extracted/2025-01-09/conn/conn.21_15_15-21_16_44.log')
 
-# Display some columns as an example
 print(df[['id.orig_h', 'id.resp_h', 'duration']])
